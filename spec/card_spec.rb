a23c1b59181c45a9a73f19e825d3a76da2834926
Juliano A. Felipe <juliano.felipe@nextt.com.br>
2021-08-10 15:11:45 -0300

    L17 refactor Card class and tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -4,10 +4,11 @@
 #
 class Card
 
-  attr_reader :type
+  attr_reader :rank, :suit
 
-  def initialize(type)
-    @type = type
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
   end
 
 end
@@ -17,11 +18,18 @@ end
 #
 
 #
-RSpec.describe 'Card' do
+RSpec.describe Card do
 
-  it 'has a type' do
-    card = Card.new('Ace of Spades')
-    expect(card.type).to eq('Ace of Spades')
+  before do
+    @card = Card.new('Ace', 'Spades')
+  end
+
+  it 'has a rank' do
+    expect(@card.rank).to eq('Ace')
+  end
+
+  it 'has a suit' do
+    expect(@card.suit).to eq('Spades')
   end
 
 end
