723e2d29a6cb2e8bc2ae32b20b88f126dab39251
Juliano A. Felipe <juliano.felipe@nextt.com.br>
2021-08-10 21:14:11 -0300

    L21 Finishes the first example of a RSpec test

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -4,7 +4,7 @@
 #
 class Card
 
-  attr_reader :rank, :suit
+  attr_accessor :rank, :suit
 
   def initialize(rank, suit)
     @rank = rank
@@ -20,16 +20,21 @@ end
 #
 RSpec.describe Card do
 
-  before do
-    @card = Card.new('Ace', 'Spades')
-  end
+  let(:card) {Card.new('Ace', 'Spades')}
 
-  it 'has a rank' do
-    expect(@card.rank).to eq('Ace')
+  it 'has a rank and that rank can change' do
+    expect(card.rank).to eq('Ace')
+    card.rank = 'Queen'
+    expect(card.rank).to eq('Queen')
   end
 
   it 'has a suit' do
-    expect(@card.suit).to eq('Spades')
+    expect(card.suit).to eq('Spades')
+  end
+
+  it 'has a custom error message' do
+    comparison = 'Spades'
+    expect(card.suit).to eq(comparison), "Hey, I got #{comparison} but I expected #{card.suit} instead"
   end
 
 end
